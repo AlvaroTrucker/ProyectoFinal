a021900e08bcef402244a1ae400ba0f78d94e49c
Alvaro <alvarojimenez95@hotmail.com>
2016-05-11 21:57:25 +0200

    Primeros pasos

---
+++ src/modelo/Persona.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class Persona {
+	//atributos
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private String genero;
+	private String pais;
+	private double dinero;
+	
+	public Persona(int id, String nombre, String apellidos, String genero, String pais, double dinero) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.genero = genero;
+		this.pais = pais;
+		this.dinero = dinero;
+	}
+	
+	
+	
+}
